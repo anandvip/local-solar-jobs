3f141647ab226a369c84a6800a0d663eab37790f
Vipul Anand <9896918475v@gmail.com>
2017-10-10 14:47:31 +0530

    Learning to use vuejs
    First app using VUEJS with Max(Meetup app)

---
+++ src/main.js
@@ -0,0 +1,41 @@
+
+import Vue from 'vue'
+
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+
+import App from './App'
+import router from './router'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
